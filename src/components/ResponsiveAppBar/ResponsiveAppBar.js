039adc9c800a8261faab947e6676e81036ffa1e6
jinalp60 <jinal_p60@yahoo.com>
2024-05-19 21:02:48 -0500

    add product page

--- src/components/ResponsiveAppBar/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.js
@@ -6,44 +6,44 @@ import styles from './ResponsiveAppBar.module.css';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Avatar from '@mui/material/Avatar';
+// import Tooltip from '@mui/material/Tooltip';
+// import MenuItem from '@mui/material/MenuItem';
+// import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Products', 'Add product', 'Update product', 'Remove product'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
   const navigate = useNavigate();
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  // const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavPage = (event) => {
     const pageToBeRouted = event.currentTarget.getAttribute("id").replace(/\s/g, "").toLowerCase()
     console.log("handleOpenNavMenu", pageToBeRouted);
     navigate(pageToBeRouted);
   };
-  const handleOpenUserMenu = (event) => {
-    console.log("handleOpenUserMenu", event.currentTarget);
-    setAnchorElUser(event.currentTarget);
-  };
+  // const handleOpenUserMenu = (event) => {
+  //   console.log("handleOpenUserMenu", event.currentTarget);
+  //   setAnchorElUser(event.currentTarget);
+  // };
 
-  const handleCloseNavMenu = () => {
-    console.log("handleCloseNavMenu");
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   console.log("handleCloseNavMenu");
+  //   setAnchorElNav(null);
+  // };
 
-  const handleCloseUserMenu = () => {
-    console.log("handleCloseUserMenu");
-    setAnchorElUser(null);
-  };
+  // const handleCloseUserMenu = () => {
+  //   console.log("handleCloseUserMenu");
+  //   setAnchorElUser(null);
+  // };
 
   return (
     <div className={styles.ResponsiveAppBar} data-testid="ResponsiveAppBar">
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ import PathConstants from "./pathConstants"
 import { Navigate } from 'react-router-dom';
 
 const PRODUCTS = [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+    { category: "Fruits", price: "1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "1", stocked: true, name: "Peas" }
 ];
 
 const Products = React.lazy(() => import("../../src/components/ProductTable/ProductTable"))
